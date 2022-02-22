168c89666cbe06bc3e2622e1949c0249016777f4
Michael Phelps <56881040+mphelps1978@users.noreply.github.com>
2022-02-22 16:34:54 -0600

    Update CognixiaApplication.java

--- src/main/java/com/example/cognixia/CognixiaApplication.java
+++ src/main/java/com/example/cognixia/CognixiaApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CognixiaApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World from Webhook again");
+		System.out.println("Hello World from Webhook again!");
 		SpringApplication.run(CognixiaApplication.class, args);
 	}
 
